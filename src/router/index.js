6296286e709c163817f6791dee39db2413c505af
Zhengyaoxu <zhengyaox@student.unimelb.edu.au>
2022-11-07 14:06:03 +0800

    upload

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import VueRouter from "vue-router"
+
+// 引入组件
+import LoginPage from "../views/loginpage/LoginPage"
+import RegisterPage from '../views/loginpage/RegisterPage'
+import Search from '../views/loginpage/Search'
+
+// 创建并暴露一个路由器
+export default new VueRouter({
+    mode: 'history',    // 路由模式，该模式不会在地址中显示井号#
+    routes: [
+        {
+            path: '/',          // 路径
+            redirect: '/login'  // 重定向
+        },
+        {
+            path: '/login',     // 路径
+            component: LoginPage    // 跳转到的组件
+        },
+        {
+            path: '/register',     // 路径
+            component: RegisterPage    // 跳转到的组件
+        },
+        {
+            path: '/search',
+            component: Search
+        },
+        
+    ]
+})
